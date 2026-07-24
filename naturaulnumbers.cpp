4d73efb228ff493833199af01d963590b17816d0
rajayan228 <rajkashyap2256@gmail.com>
2026-07-23 23:54:37 -0700

    print 1 to n numbers

---
+++ naturaulnumbers.cpp
@@ -0,0 +1,17 @@
+// program to print n natural numbers from 1 to n
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  int n,i;
+  cout<<"\nEnter a number";
+  cin >> n;
+  for(i=1;i<=n;i++)
+  cout<<i<<" ";
+  
+  return 0;
+  
+}
